eecfcf6cba0f448f2cdba72ee86e012c2af983ae
NathanNovelty <nhan.nguyentrung.dev@gmail.com>
2023-05-03 14:45:39 +0700

    submit student dao
    Create method prepare for query data from database

--- SpringFullCRUD/src/main/java/com/nhannt/dao/StudentInfoDAO.java
+++ SpringFullCRUD/src/main/java/com/nhannt/dao/StudentInfoDAO.java
@@ -1,2 +1,15 @@
-package com.nhannt.dao;public class StudentInfoDAO {
+package com.nhannt.dao;
+
+import com.nhannt.entity.StudentInfo;
+
+import java.util.List;
+
+interface StudentInfoDAO {
+    public List<StudentInfo> getListStudentInfo();
+
+    public  void saveStudentInfo( StudentInfo studentInfo);
+
+    public StudentInfo getStudentInfo(int theId);
+
+    public void deleteStudentInfo(int theId);
 }
--- SpringFullCRUD/src/main/java/com/nhannt/dao/StudentInfoDAOImp.java
+++ SpringFullCRUD/src/main/java/com/nhannt/dao/StudentInfoDAOImp.java
@@ -1,2 +1,29 @@
-package com.nhannt.dao;public class StudentInfoDAOImp {
+package com.nhannt.dao;
+
+import com.nhannt.entity.StudentInfo;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class StudentInfoDAOImp implements StudentInfoDAO{
+    @Override
+    public List<StudentInfo> getListStudentInfo() {
+        return null;
+    }
+
+    @Override
+    public void saveStudentInfo(StudentInfo studentInfo) {
+
+    }
+
+    @Override
+    public StudentInfo getStudentInfo(int theId) {
+        return null;
+    }
+
+    @Override
+    public void deleteStudentInfo(int theId) {
+
+    }
 }
